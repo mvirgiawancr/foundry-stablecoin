4fdf3deb47e3083a25ba08d1e3e2a1cb4660b77e
Virgi <virgiawan.muhamad@gmail.com>
2025-05-28 21:43:56 +0700

    add Handler

---
+++ test/fuzz/Invariants.t.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Invariants is StdInvariant, Test {
+    DSCEngine dscEngine;
+    DecentralizedStableCoin dsc;
+    HelperConfig config;
+    DeployDSC deployer;
+    address weth;
+    address wbtc;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dsc, dscEngine, config) = deployer.run();
+        (,, weth, wbtc,) = config.activeNetworkConfig();
+        targetContract(address(dscEngine));
+    }
+
+    function invariant_protocolMustHaveMoreCollateralThanTotalSupply() public view {
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dscEngine));
+        uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(dscEngine));
+
+        uint256 wethValue = dscEngine.getUsdValue(weth, totalWethDeposited);
+        uint256 wbtcValue = dscEngine.getUsdValue(wbtc, totalWbtcDeposited);
+
+        assert(wethValue + wbtcValue >= totalSupply);
+    }
+}
